6cb0e903630fa9c9b1b8a49d64c40407ee9b3db6
amalshaji <amalshajid@gmail.com>
2023-11-22 11:37:35 +0530

    update index page

--- internal/server/admin/web/src/App.svelte
+++ internal/server/admin/web/src/App.svelte
@@ -6,3 +6,23 @@
   <h2>Expose local http/ws ports to the internet</h2>
   <h3>(coming soon)</h3>
 </main>
+
+<style>
+  main {
+    padding: 0 24px;
+    margin: 0;
+    height: 80vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Inter";
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  h1,
+  h2,
+  h3 {
+    font-weight: 300;
+  }
+</style>
