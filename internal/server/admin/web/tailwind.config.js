79c608a96e2da4902760cf8a8cd152e5fa2c471b
amalshaji <amalshajid@gmail.com>
2024-01-28 12:29:43 +0530

    new logo

--- internal/server/admin/web/tailwind.config.js
+++ internal/server/admin/web/tailwind.config.js
@@ -55,7 +55,7 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["Inter var", ...fontFamily.sans],
+        sans: ["Noto Sans", ...fontFamily.sans],
       },
     },
   },
