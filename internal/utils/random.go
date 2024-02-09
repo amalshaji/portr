99d4b3f5a5ab19ca589409bc45d118be86adfcb3
amalshaji <amalshajid@gmail.com>
2024-02-09 20:38:56 +0530

    new range syntax

--- internal/utils/random.go
+++ internal/utils/random.go
@@ -4,7 +4,7 @@ import "math/rand"
 
 func GenerateRandomNumbers(start, end, limit int) []int {
 	randomNumbers := make([]int, 100)
-	for i := 0; i < 100; i++ {
+	for i := range 100 {
 		randomNumbers[i] = rand.Intn(end-start) + start
 	}
 	return randomNumbers
