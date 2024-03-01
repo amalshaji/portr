e402d088021ae4ac42731729255f69adf6ae94b2
amalshaji <amalshajid@gmail.com>
2024-03-01 20:15:03 +0530

    use rubik font

--- admin/src/web/tailwind.config.js
+++ admin/src/web/tailwind.config.js
@@ -55,7 +55,7 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["Nunito Sans", ...fontFamily.sans],
+        sans: ["Rubik", ...fontFamily.sans],
       },
     },
   },
