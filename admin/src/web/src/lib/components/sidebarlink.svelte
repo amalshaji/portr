9de989e7e87149277279c82307e1069d3cba314c
amalshaji <amalshajid@gmail.com>
2024-03-01 11:38:41 +0530

    update sidebarlink font weight

--- admin/src/web/src/lib/components/sidebarlink.svelte
+++ admin/src/web/src/lib/components/sidebarlink.svelte
@@ -7,7 +7,7 @@
 <div>
   <Link let:active to={url}>
     <div
-      class="flex text-[14px] font-normal items-center px-2 py-2 text-gray-600 rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 {active
+      class="flex text-[14px] font-extrabold items-center px-2 py-2 text-gray-600 rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 {active
         ? 'bg-gray-100 ring-1 ring-gray-300 dark:bg-gray-800 dark:text-gray-200 text-gray-900'
         : ''}"
     >
