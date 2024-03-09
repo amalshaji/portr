1590538a6c7827a650f7a0f4441e83dddb984269
amalshaji <amalshajid@gmail.com>
2024-03-09 09:37:55 +0530

    add theme switcher and issue link

---
+++ admin/src/web/src/lib/components/issue-link.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Button } from "$lib/components/ui/button";
+  import { Github } from "lucide-svelte";
+</script>
+
+<Button
+  href="https://github.com/amalshaji/portr/issues/new"
+  variant="link"
+  target="_blank"
+>
+  File an issue
+  <Github class="h-4 w-4 ml-1" />
+</Button>
---
+++ admin/src/web/src/lib/components/theme.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Sun } from "lucide-svelte";
+  import { Moon } from "lucide-svelte";
+  import { Button } from "$lib/components/ui/button/index.js";
+
+  import { toggleMode } from "mode-watcher";
+</script>
+
+<Button on:click={toggleMode} variant="ghost" size="icon">
+  <Sun
+    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+  />
+  <Moon
+    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+  />
+  <span class="sr-only">Toggle theme</span>
+</Button>
