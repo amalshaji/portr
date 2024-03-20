f3776904ddfe914ff5827e2130a7a1af220413cd
amalshaji <amalshajid@gmail.com>
2024-03-20 22:45:26 +0530

    use datetime.UTC

--- admin/src/portr_admin/beats.py
+++ admin/src/portr_admin/beats.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from portr_admin.models.auth import Session
 from portr_admin.models.connection import Connection, ConnectionStatus
 import logging
@@ -12,8 +12,8 @@ async def clear_expired_sessions():
 
 
 async def clear_unclaimed_connections():
-    logger.info(f"{datetime.utcnow()} Clearing unclaimed connections")
+    logger.info(f"{datetime.now(UTC)} Clearing unclaimed connections")
     await Connection.filter(
         status=ConnectionStatus.reserved.value,
-        created_at__lte=datetime.utcnow() - timedelta(seconds=10),
+        created_at__lte=datetime.now(UTC) - timedelta(seconds=10),
     ).delete()
