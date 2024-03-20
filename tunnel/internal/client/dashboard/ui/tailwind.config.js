92e46300acb4b726d1a817bc542b245a46039a2a
amalshaji <amalshajid@gmail.com>
2024-03-20 19:36:08 +0530

    fix client dark mode

--- tunnel/internal/client/dashboard/ui/tailwind.config.js
+++ tunnel/internal/client/dashboard/ui/tailwind.config.js
@@ -2,7 +2,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  //   darkMode: ["class"],
+  darkMode: ["class"],
   content: ["./src/**/*.{html,js,svelte,ts}"],
   safelist: ["dark"],
   theme: {
