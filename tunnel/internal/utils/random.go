7a5d488f59afb7f8987dc8a89e311de610faaf9a
amalshaji <amalshajid@gmail.com>
2024-03-28 18:28:17 +0530

    fix random number generation

--- tunnel/internal/utils/random.go
+++ tunnel/internal/utils/random.go
@@ -3,8 +3,8 @@ package utils
 import "math/rand"
 
 func GenerateRandomNumbers(start, end, limit int) []int {
-	randomNumbers := make([]int, 100)
-	for i := range 100 {
+	randomNumbers := make([]int, limit)
+	for i := range limit {
 		randomNumbers[i] = rand.Intn(end-start) + start
 	}
 	return randomNumbers
