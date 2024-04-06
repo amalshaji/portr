1dd4780837afcde5b72466825e855d37c73caf67
amalshaji <amalshajid@gmail.com>
2024-04-06 22:24:06 +0530

    feat: Add installation instructions on overview page

--- admin/src/web/src/pages/app/overview.svelte
+++ admin/src/web/src/pages/app/overview.svelte
@@ -2,7 +2,9 @@
   import { setupScript } from "$lib/store";
   import { copyCodeToClipboard } from "$lib/utils";
   import { getContext, onMount } from "svelte";
-  import "svelte-highlight/styles/stackoverflow-light.css";
+  import Highlight from "svelte-highlight";
+  import bash from "svelte-highlight/languages/bash";
+  import "svelte-highlight/styles/atom-one-light.css";
 
   const helpCommand = "portr -h";
 
@@ -17,6 +19,10 @@
     setupScript.set((await res.json())["message"]);
   };
 
+  const installCommand = `
+  brew install amalshaji/taps/portr
+  `.trim();
+
   onMount(() => {
     getSetupScript();
   });
@@ -29,31 +35,44 @@
 
   <div class="px-6 mt-2">
     <ul class="list-decimal space-y-6">
-      <li>
-        Setup up the portr client from the <a
-          href="https://portr.dev/client/installation/"
-          target="_blank"
-          class="underline">docs</a
-        >
+      <li class="space-y-2">
+        <span>Install the portr client using homebrew</span>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <span on:click={() => copyCodeToClipboard(installCommand)}>
+          <Highlight
+            language={bash}
+            code={installCommand}
+            class="border w-3/4 text-sm my-2"
+          />
+        </span>
+        <p class="mt-4 text-sm">
+          Or download the binary from the <a
+            href="https://github.com/amalshaji/portr/releases"
+            target="_blank"
+            class="underline">github releases</a
+          >
+        </p>
       </li>
       <li class="space-y-2">
         <span>Run the following command to setup portr client auth</span>
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <!-- svelte-ignore a11y-no-static-element-interactions -->
-        <div
-          class="border rounded-lg w-3/4 bg-[#F4F4F5] text-sm"
-          on:click={() => copyCodeToClipboard($setupScript)}
-        >
-          <pre class="px-4 py-3 overflow-auto">{"$ " + $setupScript}</pre>
-        </div>
+        <span on:click={() => copyCodeToClipboard($setupScript)}>
+          <Highlight
+            language={bash}
+            code={$setupScript}
+            class="border w-3/4 text-sm my-2"
+          />
+        </span>
         <p class="mt-4 text-sm">
-          Note: use <code>portr</code> instead of <code>./portr</code> if the binary
-          is set in $PATH
+          Note: use <code>./portr</code> instead of <code>portr</code> if the
+          binary is in the same folder and not set in <code>$PATH</code>
         </p>
       </li>
       <li>
         You're ready to use the tunnel, run <code
-          class="border px-2 py-1 rounded-sm">{helpCommand}</code
+          class="border px-1 py-0.5 rounded-lg">{helpCommand}</code
         >
         or checkout the
         <a
