5371824afcff979c21db89205082ee977d249bbb
amalshaji <amalshajid@gmail.com>
2024-04-20 21:37:31 +0530

    make sidebar bg visible

--- admin/src/web/src/lib/components/sidebarlink.svelte
+++ admin/src/web/src/lib/components/sidebarlink.svelte
@@ -8,7 +8,7 @@
   <Link let:active to={url}>
     <div
       class="flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary {active
-        ? 'bg-muted text-primary'
+        ? 'bg-[#f2f0f0] text-primary'
         : 'text-muted-foreground'}"
     >
       <slot />
