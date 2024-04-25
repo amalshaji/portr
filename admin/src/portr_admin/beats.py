864c15ce41d0ecef175dab498eb95b70cfac390b
amalshaji <amalshajid@gmail.com>
2024-04-25 19:58:36 +0530

    fix beat logging

--- admin/src/portr_admin/beats.py
+++ admin/src/portr_admin/beats.py
@@ -3,16 +3,16 @@ from portr_admin.models.auth import Session
 from portr_admin.models.connection import Connection, ConnectionStatus
 import logging
 
-logger = logging.getLogger("fastapi")
+logger = logging.getLogger("uvicorn.info")
 
 
 async def clear_expired_sessions():
     logger.info("Clearing expired sessions")
-    await Session.filter(expires_at__lte=datetime.utcnow()).delete()
+    await Session.filter(expires_at__lte=datetime.now(UTC)).delete()
 
 
 async def clear_unclaimed_connections():
-    logger.info(f"{datetime.now(UTC)} Clearing unclaimed connections")
+    logger.info("Clearing unclaimed connections")
     await Connection.filter(
         status=ConnectionStatus.reserved.value,
         created_at__lte=datetime.now(UTC) - timedelta(seconds=10),
