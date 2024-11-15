615f2af84caa6ff496e15bf48e75d283f0756224
Amal Shaji <amalshajid@gmail.com>
2024-11-15 21:53:42 +0530

    Handle broken tunnel connection error (#116)

--- tunnel/internal/server/proxy/proxy.go
+++ tunnel/internal/server/proxy/proxy.go
@@ -81,10 +81,17 @@ func unregisteredSubdomainError(w http.ResponseWriter, subdomain string) {
 	w.Write([]byte(utils.UnregisteredSubdomain(subdomain)))
 }
 
+func connectionLostError(w http.ResponseWriter) {
+	w.Header().Set("X-Portr-Error", "true")
+	w.Header().Set("X-Portr-Error-Reason", "connection-lost")
+	w.WriteHeader(http.StatusServiceUnavailable)
+	w.Write([]byte(utils.ConnectionLost()))
+}
+
 func (p *Proxy) ErrHandle(res http.ResponseWriter, req *http.Request, err error) {
 	log.Error("Error from proxy", "error", err)
 	p.RemoveRoute(p.config.ExtractSubdomain(req.Host))
-	unregisteredSubdomainError(res, p.config.ExtractSubdomain(req.Host))
+	connectionLostError(res)
 }
 
 func (p *Proxy) reverseProxy(target *url.URL) *httputil.ReverseProxy {
