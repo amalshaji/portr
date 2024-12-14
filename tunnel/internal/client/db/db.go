a0431da4a226d3520326807234de8e1d9ea9c55c
amalshaji <amalshajid@gmail.com>
2024-12-14 11:20:39 +0530

    Add sqlite pragmas for local db

--- tunnel/internal/client/db/db.go
+++ tunnel/internal/client/db/db.go
@@ -17,11 +17,24 @@ type Db struct {
 	Conn *gorm.DB
 }
 
+var SQLITE_PRAGMAS = []string{
+	`PRAGMA foreign_keys = ON;`,
+	`PRAGMA journal_mode = WAL;`,
+	`PRAGMA synchronous = NORMAL;`,
+	`PRAGMA busy_timeout = 5000;`,
+	`PRAGMA temp_store = MEMORY;`,
+	`PRAGMA mmap_size = 134217728;`,
+	`PRAGMA journal_size_limit = 67108864;`,
+	`PRAGMA cache_size = 2000;`,
+}
+
 func New(config *config.Config) *Db {
 	homeDir, _ := os.UserHomeDir()
 
 	gormConfig := &gorm.Config{
 		SkipDefaultTransaction: true,
+		TranslateError:         true,
+		PrepareStmt:            true,
 	}
 	if !config.Debug {
 		gormConfig.Logger = logger.Default.LogMode(logger.Silent)
@@ -30,6 +43,18 @@ func New(config *config.Config) *Db {
 	db, err := gorm.Open(sqlite.Open(homeDir+"/.portr/db.sqlite"), gormConfig)
 	if err != nil {
 		log.Fatal("Failed to connect database", "error", err)
+	}
+
+	conn, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database connection", "error", err)
+	}
+
+	for _, pragma := range SQLITE_PRAGMAS {
+		_, err = conn.Exec(pragma)
+		if err != nil {
+			log.Fatal("Failed to set pragma", "error", err)
+		}
 	}
 
 	db.AutoMigrate(&Request{})
