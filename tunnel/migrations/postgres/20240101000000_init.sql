af83b0f642983731a1826c2fa66a5d2df128ff08
amalshaji <amalshajid@gmail.com>
2025-09-08 02:58:26 +0530

    Use timestampz in postgres migration

--- tunnel/migrations/postgres/20240101000000_init.sql
+++ tunnel/migrations/postgres/20240101000000_init.sql
@@ -3,8 +3,8 @@
 CREATE TABLE
     IF NOT EXISTS "user" (
         "id" SERIAL PRIMARY KEY,
-        "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         "email" TEXT NOT NULL UNIQUE,
         "first_name" TEXT,
         "last_name" TEXT,
@@ -16,10 +16,10 @@ CREATE TABLE
 CREATE TABLE
     IF NOT EXISTS "session" (
         "id" SERIAL PRIMARY KEY,
-        "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         "token" TEXT NOT NULL UNIQUE,
-        "expires_at" TIMESTAMP NOT NULL,
+        "expires_at" TIMESTAMPTZ NOT NULL,
         "user_id" INTEGER NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE
     );
 
@@ -29,8 +29,8 @@ CREATE INDEX IF NOT EXISTS "idx_session_expires_at" ON "session" ("expires_at");
 CREATE TABLE
     IF NOT EXISTS "githubuser" (
         "id" SERIAL PRIMARY KEY,
-        "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         "github_id" BIGINT NOT NULL UNIQUE,
         "github_access_token" TEXT NOT NULL,
         "github_avatar_url" TEXT NOT NULL,
@@ -43,8 +43,8 @@ CREATE INDEX IF NOT EXISTS "idx_githubuser_github_id" ON "githubuser" ("github_i
 CREATE TABLE
     IF NOT EXISTS "team" (
         "id" SERIAL PRIMARY KEY,
-        "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         "name" TEXT NOT NULL UNIQUE,
         "slug" TEXT NOT NULL UNIQUE
     );
@@ -55,8 +55,8 @@ CREATE INDEX IF NOT EXISTS "idx_team_slug" ON "team" ("slug");
 CREATE TABLE
     IF NOT EXISTS "team_users" (
         "id" SERIAL PRIMARY KEY,
-        "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         "secret_key" TEXT NOT NULL UNIQUE,
         "role" TEXT NOT NULL DEFAULT 'member',
         "team_id" INTEGER NOT NULL REFERENCES "team" ("id") ON DELETE CASCADE,
@@ -68,15 +68,15 @@ CREATE INDEX IF NOT EXISTS "idx_team_users_secret_key" ON "team_users" ("secret_
 -- Create connection table
 CREATE TABLE
     IF NOT EXISTS "connection" (
-        "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
         "id" TEXT NOT NULL PRIMARY KEY,
         "type" TEXT NOT NULL,
         "subdomain" TEXT,
         "port" INTEGER,
         "status" TEXT NOT NULL DEFAULT 'reserved',
-        "started_at" TIMESTAMP,
-        "closed_at" TIMESTAMP,
+        "started_at" TIMESTAMPTZ,
+        "closed_at" TIMESTAMPTZ,
         "created_by_id" INTEGER NOT NULL REFERENCES "team_users" ("id") ON DELETE CASCADE,
         "team_id" INTEGER NOT NULL REFERENCES "team" ("id") ON DELETE CASCADE
     );
