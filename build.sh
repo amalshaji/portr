626b79c6cd3e7776dec6b21a7265dc567ffb6eb2
regbo <reggie.pierce@gmail.com>
2026-02-04 21:05:12 -0500

    recipe

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+pushd ./tunnel/internal/client/dashboard/ui
+corepack enable >/dev/null 2>&1 || true
+pnpm install
+pnpm build
+popd
 
-cd /Users/reggie.pierce/Projects/github-regbo/portr/tunnel
+pushd ./tunnel
+go test ./...
+popd
+
+pushd ./tunnel
 go build -o ./bin/portr ./cmd/portr
 go build -o ./bin/portrd ./cmd/portrd
+popd
